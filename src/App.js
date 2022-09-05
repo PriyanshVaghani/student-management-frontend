7f5122703fb20a1ce26acb234d2c00f79172f898
PriyanshVaghani <88379030+PriyanshVaghani@users.noreply.github.com>
2022-09-05 16:51:35 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import NavBar from "./Components/NavBar";
 import Student from "./Components/Student";
 import StudentList from "./Components/StudentList";
 import Container from "react-bootstrap/Container";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
+    return (<div className="App">
+        <Router>
             <NavBar/>
             <Container>
-                <Student/>
-                <StudentList/>
+                <Routes>
+                    <Route path="student" element={<Student/>}/>
+                    <Route path="listStudents" element={<StudentList/>}/>
+                </Routes>
             </Container>
             <Footer/>
-        </div>
-    );
+        </Router>
+    </div>);
 }
 
 export default App;
